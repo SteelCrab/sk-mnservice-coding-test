5fb454aad4d9e2f7ed999a0e1d16c27a2e2770c0
박용현 <pyh5523@gmail.com>
2025-07-03 12:51:40 +0900

    feat(task-2): React 메인 애플리케이션 구조 구현
    - main.jsx: React 진입점 설정
    - App.jsx: 메인 애플리케이션 컴포넌트
    - index.css: 원본 스타일 완전 유지
    - React 18 StrictMode 적용

---
+++ 26865523-task-2/fix/과제2_이름검색_시스템_개선_react/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import NameSearchSystem from './components/NameSearchSystem';
+
+function App() {
+  return (
+    <div className="App">
+      <NameSearchSystem />
+    </div>
+  );
+}
+
+export default App;
